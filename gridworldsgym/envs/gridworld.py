761ed78cf9a96ff22cca9ce8d03ab2eedf5f9232
Miguel Alonso Jr <drmiguelalonsojr@gmail.com>
2019-10-08 12:49:55 -0400

    Updates for value and reward display during rendering.

--- gridworldsgym/envs/gridworld.py
+++ gridworldsgym/envs/gridworld.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pyglet
 
 from gridworldsgym.envs.discrete import FiniteStateMDP
 
@@ -22,39 +23,50 @@ class GridWorldV0(FiniteStateMDP):
         slipping.
     """
 
+    def step(self, action):
+        state, reward, done, info = super(GridWorldV0, self).step(action)
+        self.cum_reward += reward
+        return state, reward, done, info
+
     def close(self):
         if self.viewer:
             self.viewer.close()
             self.viewer = None
 
-    def render(self, mode='human'):
-        # TODO: add option to show cumulative rewards
-        # TODO: add option to show value function
-        screen_width = 100 * self.width
-        screen_height = 100 * self.height
+    def render(self, mode='human', values=None):
+        lw = 5
+        square_size = 100
+        screen_width = square_size * self.width + lw
+        screen_height_init = square_size * self.height
+        height_multiplier = (1.1 if self.show_reward and values is None else 1)
+        screen_height = int(screen_height_init * height_multiplier) + lw
+        diff = screen_height - screen_height_init
 
         self._set_heading(self.last_action)
 
         def get_x_y(row, col):
-            return col * 100 + 50, screen_height - (row * 100 + 50)
+            return col * square_size + 50 + lw / 2, screen_height - (row * square_size + 50) - diff + lw / 2
 
         if self.viewer is None:
-            from gym.envs.classic_control import rendering
+            # from gym.envs.classic_control import rendering
+            from gridworldsgym.util import rendering
             self.viewer = rendering.Viewer(screen_width, screen_height)
-            line_width = rendering.LineWidth(5)
-            for i in range(1, self.width):
-                line = rendering.Line((i * 100, 0), (i * 100, screen_height))
+            line_width = rendering.LineWidth(lw)
+
+            for i in range(0, self.width + 1):
+                line = rendering.Line((i * square_size + lw / 2, 0), (i * square_size + lw / 2, screen_height_init))
                 line.set_color(0, 0, 0)
                 line.attrs[1] = line_width
                 self.viewer.add_geom(line)
-            for j in range(1, self.height):
-                line = rendering.Line((0, j * 100), (screen_width, j * 100))
+
+            for j in range(0, self.height + 1):
+                line = rendering.Line((0, j * square_size + lw / 2), (screen_width, j * square_size + lw / 2))
                 line.set_color(0, 0, 0)
                 line.attrs[1] = line_width
                 self.viewer.add_geom(line)
 
             for row, col in self.goal_states:
-                goal = rendering.FilledPolygon([(-49, -49), (-49, 49), (49, 49), (49, -49)])
+                goal = rendering.FilledPolygon([(-48, -48), (-48, 48), (48, 48), (48, -48)])
                 goal.set_color(0.0, 1.0, 0.0)
                 goal_transform = rendering.Transform()
                 goal.add_attr(goal_transform)
@@ -63,8 +75,17 @@ class GridWorldV0(FiniteStateMDP):
                 self.viewer.add_geom(goal)
 
             for row, col in self.terminal_states:
-                goal = rendering.FilledPolygon([(-49, -49), (-49, 49), (49, 49), (49, -49)])
+                goal = rendering.FilledPolygon([(-48, -48), (-48, 48), (48, 48), (48, -48)])
                 goal.set_color(1.0, 0.0, 0.0)
+                goal_transform = rendering.Transform()
+                goal.add_attr(goal_transform)
+                new_x, new_y = get_x_y(row, col)
+                goal_transform.set_translation(new_x, new_y)
+                self.viewer.add_geom(goal)
+
+            for row, col in self.illegal_states:
+                goal = rendering.FilledPolygon([(-48, -48), (-48, 48), (48, 48), (48, -48)])
+                goal.set_color(0.25, 0.25, 0.25)
                 goal_transform = rendering.Transform()
                 goal.add_attr(goal_transform)
                 new_x, new_y = get_x_y(row, col)
@@ -74,18 +95,42 @@ class GridWorldV0(FiniteStateMDP):
             agent_size = 50
             l, r, t, b = -agent_size / 2, agent_size / 2, agent_size / 2, -agent_size / 2
             x, y = get_x_y(0, 0)
-            self.agent_transform = rendering.Transform(translation=(x, y))
+            self.agent_transform = rendering.Transform(translation=(x, y), rotation=self.heading)
             agent = rendering.FilledPolygon([(l, b), (0.0, t), (0.0 * r, t), (r, b)])
             agent.set_color(95 / 256, 125 / 256, 153 / 256)
             agent.add_attr(self.agent_transform)
-            self.viewer.add_geom(agent)
+            if values is None:
+                self.viewer.add_geom(agent)
+
+            if values is not None:
+                self.labels = []
+                for state in range(self.num_states):
+                    s_row, s_col = self.to_row_col(state)
+                    s_x, s_y = get_x_y(s_row, s_col)
+                    label = rendering.Label(s_x, s_y)
+                    self.viewer.add_geom(label)
+                    self.labels.append(label)
+
+            if self.show_reward and values is None:
+                reward_label = rendering.Label(200, screen_height - 2 * lw)
+                self.reward_label = reward_label
+                self.viewer.add_geom(reward_label)
+
+        if values is not None:
+            for state in range(self.num_states):
+                self.labels[state].label.text = str(round(values[state], 2))
+
+        if self.show_reward and values is None:
+            self.reward_label.label.text = "Cumulative reward: " + str(self.cum_reward)
 
         row, col = self.to_row_col(self.state)
         new_x, new_y = get_x_y(row, col)
-        self.agent_transform.set_translation(new_x, new_y)
+        self.agent_transform.set_translation(new_x, new_y - lw)
         self.agent_transform.set_rotation(self.heading)
 
-        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
+        arr = self.viewer.render(return_rgb_array=mode == 'rgb_array')
+
+        return arr
 
     def __init__(self, width=4, height=3, slippery=False):
         self.width = width
@@ -104,6 +149,8 @@ class GridWorldV0(FiniteStateMDP):
         self.rewards = self._generate_rewards()
         self.viewer = None
         self.heading = NORTH
+        self.show_reward = False
+        self.cum_reward = 0.0
 
     def _to_state(self, row, col):
         return row * self.width + col
